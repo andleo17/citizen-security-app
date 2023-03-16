81e9243cbcad25877aed1bef89a5974a230b9066
andleo17 <andres.gastulo@gmail.com>
2023-03-16 13:46:45 -0500

    fixing pagination style

--- resources/js/Components/Common/Table.tsx
+++ resources/js/Components/Common/Table.tsx
@@ -7,7 +7,7 @@ interface RowProps {
 }
 
 interface PaginationProps {
-  info: Omit<Paginable, "data">;
+  info?: Omit<Paginable, "data">;
 }
 
 function Head({ children }: PropsWithChildren) {
@@ -80,7 +80,7 @@ function Pagination({ info }: PaginationProps) {
                 fillRule="evenodd"
                 d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
                 clipRule="evenodd"
-              ></path>
+              />
             </svg>
           </Link>
         </li>
@@ -90,8 +90,8 @@ function Pagination({ info }: PaginationProps) {
               href={l.url}
               className={
                 l.active
-                  ? "z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                  : "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  ? "block z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                  : "block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               }
             >
               {l.label}
@@ -117,7 +117,7 @@ function Pagination({ info }: PaginationProps) {
                 fillRule="evenodd"
                 d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                 clipRule="evenodd"
-              ></path>
+              />
             </svg>
           </Link>
         </li>
